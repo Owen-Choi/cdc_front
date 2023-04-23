4de2c30e3ba0525fc8384a3eb3150aa785f27dbd
Owen-Choi <oldstyle482@gmail.com>
2023-04-23 17:40:57 +0900

    [ADD] 디자인 수정

--- rest_ex/src/App.js
+++ rest_ex/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios'
 
 function App() {
@@ -33,30 +36,45 @@ function App() {
     };
 
   return (  
-    <>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Id: </label>
-        <input type="text" name="id" value={data.id} onChange={handleChange} />
-      </div>
-      <div>
-        <label>Name: </label>
-        <input type="text" name="name" value={data.name} onChange={handleChange} />
-      </div>
-      <div>
-        <label>Age: </label>
-        <input type="text" name="age" value={data.age} onChange={handleChange} />
-      </div>
-      <div>
-        <label>Email: </label>
-        <input type="text" name="email" value={data.email} onChange={handleChange} />
-      </div>
-      <button type="submit">Insert Data</button>
-    </form>
+    // <>
+    <Card bg='dark' border='light' style={{width: "300px"}}>
+      <Card.Body>        
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label style={{color: 'white', marginRight: '39px'}}>Id: </label>
+            <input style={{display: 'inline-block'}} type="text" name="id" value={data.id} onChange={handleChange} />
+          </div>
+          <div>
+            <label style={{color: 'white', marginRight: '10px'}}>Name: </label>
+            <input style={{display: 'inline-block'}} type="text" name="name" value={data.name} onChange={handleChange} />
+          </div>
+          <div>
+            <label style={{color: 'white', marginRight: '24.5px'}}>Age: </label>
+            <input style={{display: 'inline-block'}} type="text" name="age" value={data.age} onChange={handleChange} />
+          </div>
+          <div>
+            <label style={{color: 'white', marginRight: '15px'}}>Email: </label>
+            <input style={{display: 'inline-block'}} type="text" name="email" value={data.email} onChange={handleChange} />
+          </div>
+          <div style={{marginTop: '30px'}}></div>
+          <Button variant='primary' type="submit">Insert Data</Button>    
+        </form>
+      </Card.Body>
+    {/* </Card> */}
+    {/* {submit ?
+    <div style={{color: 'white'}}> Name : {data.name} Age : {data.age} Email : {data.email}</div>  
+    : <></>} */}
     {submit ?
-    <div> Name : {data.name} Age : {data.age} Email : {data.email}</div>  
+    <Card bg='success'> 
+      <Card.Body>
+        <Card.Title style={{color: 'white'}}>데이터 입력</Card.Title>
+          <Card.Text style={{color: 'white'}}>
+           Name : {data.name} Age : {data.age} Email : {data.email}
+          </Card.Text>
+        </Card.Body>
+    </Card>  
     : <></>}
-    </>
+    </Card>
     );
     
 }
